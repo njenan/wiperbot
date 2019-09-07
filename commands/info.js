705c81eba9f2cb140e9e2a478dd1fe363b31ab87
kiddynamo <njenan@gmail.com>
2019-09-06 21:13:58 -0700

    fixing missing logger in info command

--- commands/info.js
+++ commands/info.js
@@ -1,37 +1,39 @@
 let exec = require('child_process').exec
 
-module.exports = (message) => {
-  exec('git rev-parse HEAD', (err, gitSha) => {
-    if (err) {
-      gitSha = 'ERR';
-      logger.error(err);
-    }
-
-    exec('node -v', (err, nodeVersion) => {
+module.exports = logger => {
+  return (message) => {
+    exec('git rev-parse HEAD', (err, gitSha) => {
       if (err) {
-        nodeVersion = 'ERR';
+        gitSha = 'ERR';
         logger.error(err);
       }
 
-      exec('dig +short myip.opendns.com @resolver1.opendns.com', (err, ip) => {
+      exec('node -v', (err, nodeVersion) => {
         if (err) {
-          ip = 'ERR';
+          nodeVersion = 'ERR';
           logger.error(err);
         }
 
-        exec('uname', (err, os) => {
-          if (err) {
-            os = 'ERR';
-            logger.error(err);
-          }
+        exec('dig +short myip.opendns.com @resolver1.opendns.com',
+             (err, ip) => {
+               if (err) {
+                 ip = 'ERR';
+                 logger.error(err);
+               }
 
-          exec('uname -r', (err, osv) => {
-            if (err) {
-              osv = 'ERR';
-              logger.error(err);
-            }
+               exec('uname', (err, os) => {
+                 if (err) {
+                   os = 'ERR';
+                   logger.error(err);
+                 }
 
-            message.channel.send(`Wiper Bot Info:\`\`\`
+                 exec('uname -r', (err, osv) => {
+                   if (err) {
+                     osv = 'ERR';
+                     logger.error(err);
+                   }
+
+                   message.channel.send(`Wiper Bot Info:\`\`\`
 os: ${os.replace('\n', '')}
 os version: ${osv.replace('\n', '')}
 bot version: ${gitSha.replace('\n', '')}
@@ -39,9 +41,10 @@ node version: ${nodeVersion.replace('\n', '')}
 server time: ${new Date().toTimeString()}
 ip: ${ip}
 \`\`\``);
-          });
-        });
+                 });
+               });
+             });
       });
     });
-  });
-}
+  };
+};
